951dd618eef8679b796822137feaf14fd09bc140
eduardoperezlutin <eduardoperezlutin@gmail.com>
2018-10-05 02:28:29 -0500

    Add Loader functionality with React

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = {
+        loading: true
+    };
+    
+    componentDidMount() {
+        setTimeout( () => this.setState( { loading: false } ), 1500);
+    }
+
     render() {
+        if (this.state.loading) {
+            return (
+                <div id="loading">
+                    <div className="load-circle"><span className="one"></span></div>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 Arly Stores Page with React
