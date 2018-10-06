815d38af4e229eab2fc20ef549a4f9347dd7f0a0
eduardoperezlutin <eduardoperezlutin@gmail.com>
2018-10-05 20:46:21 -0500

    Manage state for Header

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 // components
 import Navigation from '../components/Navigation/Navigation';
+import Header from '../components/Header/Header';
 import Main from '../components/Main/Main';
 
 class App extends Component {
     state = {
-        loading: true
+        loading: true,
+        headerInfo: {
+            title: "Artificial intelligence as a community manager",
+            description: "Whether you want to start your business, scale or reduce your operating costs, Arly's platform helps you in the process.Imagine the concept of renting a place for your business, in Arly you can rent the online store, the staff and the distribution logistics.",
+            background: 'backgrounds/bg-effect-1.svg',
+            banner: "banners/banner1.png",
+            buttonLink: '#price',
+            buttonText: 'Limited Availability'
+        }
     };
     
     componentDidMount() {
@@ -25,11 +34,12 @@ class App extends Component {
         return (
             <div className="App">
                 <Navigation />
-                <Main />
+                <Main>
+                    <Header isHome={true} headerInfo={this.state.headerInfo}/>
+                </Main>
             </div>
         );
     }
 }
     
 export default App;
-    
