a618e9f901e449d1ae5e9ffdc61cbf5685c41e1e
eduardoperezlutin <eduardoperezlutin@gmail.com>
2018-10-07 02:33:31 -0500

    Fix Dropdowns

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const faq = (props) => {
-    const labelledby = "faqa-" + props.number;
-    const id = "faqc-" + props.number;
-    const target = "#" + id;
+class Faq extends Component {
+    constructor() {
+        super();
+        this.state = {
+            openDropdown: false
+        }
+    }
 
-    return (
-        <div className="faq-row">
-            <div className="faq-header" id={labelledby}>
-                <h5 className="faq-title" data-toggle="collapse" data-target={target} data-aria-expanded="true" data-aria-controls="faqc-1">
-                <span className="no theme-g-bg">{props.number}</span> {props.question}
-                </h5>
-            </div>
-            <div id={id} className="collapse" aria-labelledby={labelledby} data-parent="#accordion">
-                <div className="faq-body">
-                    <div dangerouslySetInnerHTML={{ __html: props.answer }} />
+    openDropdown = () => {
+        const prevState = {...this.state};
+        this.setState({
+            openDropdown: !prevState.openDropdown
+        })
+    }
+
+    render() {
+        const labelledby = "faqa-" + this.props.number;
+        const id = "faqc-" + this.props.number;
+        const target = "#" + id;
+        let dropdownClass = "collapse"
+
+        if (this.state.openDropdown) {
+            dropdownClass += " show";
+        }
+
+        return (
+            <div className="faq-row">
+                <div onClick={this.openDropdown} className="faq-header" id={labelledby}>
+                    <h5 className="faq-title" data-toggle="collapse" data-target={target} data-aria-expanded="true" data-aria-controls="faqc-1">
+                    <span className="no theme-g-bg">{this.props.number}</span> {this.props.question}
+                    </h5>
+                </div>
+                <div id={id} className={dropdownClass} aria-labelledby={labelledby} data-parent="#accordion">
+                    <div className="faq-body">
+                        <div dangerouslySetInnerHTML={{ __html: this.props.answer }} />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default faq;
+export default Faq;
